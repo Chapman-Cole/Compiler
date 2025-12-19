2acb67910784d0b8ad583ed34a481ded70fc256c
Cole Chapman <colechapman924@gmail.com>
2025-12-18 21:28:38 -0800

    Improved visualizer, but scrolling needs work for drag>1

--- src/visualizer.c
+++ src/visualizer.c
@@ -1,6 +1,11 @@
+#include "DynamicArray.h"
 #include "Strings.h"
+#include "math.h"
 #include "raylib.h"
+#include "raymath.h"
+#include <math.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 // x_pos and y_pos refer to the center of the text_box
 // everything is based on percentages, except for font size, to make scaling work properly
@@ -16,7 +21,7 @@ typedef struct text_box {
     // one of the values in the text_box_positions enum
     int text_pos;
     int padding;
-    Font font;
+    Font* font;
     float spacing;
 } text_box;
 
@@ -36,11 +41,9 @@ enum text_box_positions {
 // the string that is passed in
 int text_box_init(text_box* tbox, string text);
 
-int text_box_set_font(text_box* tbox, string path);
-
 int text_box_destroy(text_box* tbox);
 
-int text_box_render(text_box* tbox, Vector2 transform, float zoom);
+int text_box_render(text_box* tbox);
 
 float lerp(float a, float b, float t) {
     return a + t * (b - a);
@@ -51,37 +54,50 @@ int height = 450;
 float aspect_ratio = (float)450 / (float)800;
 
 bool leftClickHold = false;
-Vector2 prevMouse = { 0.0f, 0.0f };
-Vector2 currMouse = { 0.0f, 0.0f };
-Vector2 prevView = { 0.0f, 0.0f };
-float scroll_factor = 0.075f;
+Vector2 prevMouse = {0.0f, 0.0f};
+Vector2 currMouse = {0.0f, 0.0f};
+Vector2 prevView = {0.0f, 0.0f};
+float drag_factor = 0.8f;
+float scroll_factor = 0.2f;
 
-// Used for translation later on
-Vector2 viewPos = { 0.0f, 0.0f };
-
-float zoom_factor = 1.0f;
+DynamicArray fonts;
 
 int main(void) {
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
     InitWindow(width, height, "raylib [core] example - basic window");
 
+    dynamic_array_registry_type_append(&STRING("Font"), NULL, sizeof(Font));
+    dynamic_array_init(&fonts, &STRING("Font"));
+
+    Font default_font, font1, font2;
+    default_font = GetFontDefault();
+    font1 = LoadFontEx("/usr/share/fonts/TTF/JetBrainsMonoNerdFont-Bold.ttf", 64, NULL, 0);
+    font2 = LoadFontEx("/usr/share/fonts/Adwaita/AdwaitaMono-Bold.ttf", 64, NULL, 0);
+    dynamic_array_append(&fonts, &default_font);
+    dynamic_array_append(&fonts, &font1);
+    dynamic_array_append(&fonts, &font2);
+
     text_box text;
     text_box_init(&text, STRING("Sample Text\nNewline"));
-    text_box_set_font(&text, STRING("/usr/share/fonts/TTF/JetBrainsMonoNerdFont-Bold.ttf"));
     text.pos = (Vector2){25, 50};
+    text.font = (Font*)dynamic_array_get(&fonts, &INDEX(1));
 
     text_box text2;
     text_box_init(&text2, STRING("Some Sample\nText!"));
-    text_box_set_font(&text2, STRING("/usr/share/fonts/Adwaita/AdwaitaMono-Bold.ttf"));
     text2.pos = (Vector2){75, 50};
+    text2.font = (Font*)dynamic_array_get(&fonts, &INDEX(2));
 
     text_box rect;
     text_box_init(&rect, STRING(""));
     rect.background = (Color){255, 128, 128, 255};
 
-    while (!WindowShouldClose()) {
-        BeginDrawing();
+    Camera2D camera = {0};
+    camera.target = (Vector2){0.0f, 0.0f};
+    camera.offset = (Vector2){0.0f, 0.0f};
+    camera.rotation = 0.0f;
+    camera.zoom = 1.0f;
 
+    while (!WindowShouldClose()) {
         if (IsWindowResized()) {
             width = GetScreenWidth();
             height = GetScreenHeight();
@@ -90,17 +106,16 @@ int main(void) {
 
         if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
             if (leftClickHold == false) {
-                prevMouse = GetMousePosition();
-                prevView = viewPos;
+                prevMouse = GetScreenToWorld2D(GetMousePosition(), camera);
+                prevView = camera.target;
                 leftClickHold = true;
             }
 
-            currMouse = GetMousePosition();
+            currMouse = GetScreenToWorld2D(GetMousePosition(), camera);
 
-            viewPos = (Vector2){ 
-                .x = prevView.x + scroll_factor * (prevMouse.x - currMouse.x),
-                .y = prevView.y + scroll_factor * (prevMouse.y - currMouse.y)
-            };
+            camera.target = (Vector2){
+                .x = prevView.x + drag_factor * (prevMouse.x - currMouse.x),
+                .y = prevView.y + drag_factor * (prevMouse.y - currMouse.y)};
 
         } else {
             leftClickHold = false;
@@ -108,16 +123,27 @@ int main(void) {
 
         float mouseScroll = GetMouseWheelMove();
         if (mouseScroll != 0) {
-            zoom_factor += 0.05f * mouseScroll;
-            if (zoom_factor < 0.0f) {
-                zoom_factor = 0.0f;
-            }
-        }
+            Vector2 mousePos = GetMousePosition();
+            Vector2 preMousePos = GetScreenToWorld2D(mousePos, camera);
+
+            camera.target = Vector2Subtract(camera.target, preMousePos);
+            camera.zoom = Clamp(expf(logf(camera.zoom) + scroll_factor * mouseScroll), 0.125f, 64.0f);
+            Vector2 postMousePos = GetScreenToWorld2D(mousePos, camera);
+            camera.target = Vector2Add(camera.target, preMousePos);
+            camera.target = Vector2Subtract(camera.target, postMousePos);
+         }
+
+        BeginDrawing();
 
         ClearBackground((Color){203, 195, 227, 150});
-        text_box_render(&rect, (Vector2){0.0f, 0.0f}, 1.0f);
-        text_box_render(&text, viewPos, zoom_factor);
-        text_box_render(&text2, viewPos, zoom_factor);
+
+        BeginMode2D(camera);
+
+        text_box_render(&rect);
+        text_box_render(&text);
+        text_box_render(&text2);
+
+        EndMode2D();
 
         EndDrawing();
     }
@@ -126,6 +152,7 @@ int main(void) {
     text_box_destroy(&text);
     text_box_destroy(&text2);
     text_box_destroy(&rect);
+    dynamic_array_free(&fonts);
     return 0;
 }
 
@@ -135,19 +162,14 @@ int text_box_init(text_box* tbox, string text) {
 
     // Initialize the rest to some default values that can be edited directly in the struct
     tbox->background = WHITE;
-    tbox->font = GetFontDefault();
+    tbox->font = (Font*)dynamic_array_get(&fonts, &INDEX(0));
     tbox->font_size = 12.0f;
-    tbox->pos = (Vector2){ .x = 50, .y = 50 };
-    tbox->dimensions = (Vector2){ .x = 30, .y = 30};
+    tbox->pos = (Vector2){.x = 50, .y = 50};
+    tbox->dimensions = (Vector2){.x = 30, .y = 30};
     tbox->padding = 0.0f;
     tbox->text_color = BLACK;
     tbox->text_pos = TBOX_CENTER;
     tbox->spacing = 1.0f; // 1 is normal character spacing
-    return 0;
-}
-
-int text_box_set_font(text_box* tbox, string path) {
-    tbox->font = LoadFontEx(path.str, 32, NULL, 0);
     return 0;
 }
 
@@ -156,22 +178,22 @@ int text_box_destroy(text_box* tbox) {
     return 0;
 }
 
-int text_box_render(text_box* tbox, Vector2 transform, float zoom) {
+int text_box_render(text_box* tbox) {
     // DrawRectangle refers to the top left corner in raylib, so transformations
     // must be done to make it refer to the center of the rectangle (text box)
-    float width_transl = (int)((tbox->dimensions.x / 100.0f * zoom) * (float)width * aspect_ratio);
-    float height_transl = (int)((tbox->dimensions.y / 100.0f * zoom) * (float)height);
-    float x_transl = (int)(((tbox->pos.x - transform.x)) / 100.0f * (float)width) - (width_transl / 2.0f);
-    float y_transl = (int)(((tbox->pos.y - transform.y)) / 100.0f * (float)height) - (height_transl / 2.0f);
+    float width_transl = (int)((tbox->dimensions.x / 100.0f) * (float)width * aspect_ratio);
+    float height_transl = (int)((tbox->dimensions.y / 100.0f) * (float)height);
+    float x_transl = (int)(((tbox->pos.x)) / 100.0f * (float)width) - (width_transl / 2.0f);
+    float y_transl = (int)(((tbox->pos.y)) / 100.0f * (float)height) - (height_transl / 2.0f);
 
     // Make font_size relative for better scaling
     float font_scaling_constant = 0.002f;
-    int font_size_transl = (int)((float)tbox->font_size * (float)height * font_scaling_constant * zoom);
+    int font_size_transl = (int)((float)tbox->font_size * (float)height * font_scaling_constant);
 
     DrawRectangle(x_transl, y_transl, width_transl, height_transl, tbox->background);
     switch (tbox->text_pos) {
         case TBOX_CENTER:
-            DrawTextEx(tbox->font, tbox->text.str, (Vector2){x_transl, y_transl}, font_size_transl, tbox->spacing, tbox->text_color);
+            DrawTextEx(*tbox->font, tbox->text.str, (Vector2){x_transl, y_transl}, font_size_transl, tbox->spacing, tbox->text_color);
             break;
 
         case TBOX_CENTER_LEFT:
